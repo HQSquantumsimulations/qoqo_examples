f93b8719c39d0e58fa070444fc5b9705418e9a36
Marina Walt <marina.walt@quantumsimulations.com>
2022-03-14 10:56:52 +0000

    fixed clippy issues

--- roqoqo/standalone/src/intro_to_roqoqo.rs
+++ roqoqo/standalone/src/intro_to_roqoqo.rs
@@ -3,16 +3,15 @@ extern crate roqoqo_quest;
 extern crate serde_json;
 
 use roqoqo::backends::{EvaluatingBackend, RegisterResult};
-use roqoqo::measurements::{BasisRotationInput, BasisRotation};
+use roqoqo::measurements::{PauliZProduct, PauliZProductInput};
 use roqoqo::{operations::*, registers::*, Circuit, QuantumProgram};
 use roqoqo_quest::Backend;
 use std::collections::{HashMap, HashSet};
 
 // Introduction examples for simple circuits and measurements.
-// For extended examples on "Fine control over decoherence", usage of "Symbolic parameters", 
-// or "Testing performance with qoqo_mock" backend, 
+// For extended examples on "Fine control over decoherence", usage of "Symbolic parameters",
+// or "Testing performance with qoqo_mock" backend,
 // please refer to Intro examples in Jupyter notebooks.
-
 
 /// Example to entangle a circuit snippet.
 ///
@@ -30,12 +29,8 @@ pub fn entangling_circuit_snippet() {
 
     // Print
     println!("{:?}", circuit);
-    println!("{} {:?}", "Circuit length: ", circuit.len());
-    println!(
-        "{} {:?}",
-        "Operation types: ",
-        circuit.get_operation_types(),
-    );
+    println!("Circuit length: {:?}", circuit.len());
+    println!("Operation types: {:?}", circuit.get_operation_types(),);
 
     // For demonstrative purposes only:
     // Compare obtained operation types to those expected for this example
@@ -64,10 +59,10 @@ pub fn measuring_qubits() {
     circuit += Hadamard::new(0);
     circuit += CNOT::new(0, 1);
     // Add operation to the circuit to perform repeated measurements in a quantum computing simulation.
-    circuit += PragmaRepeatedMeasurement::new(register_name.clone(), 10, None);
+    circuit += PragmaRepeatedMeasurement::new(register_name, 10, None);
     println!(
-        "{} {:?}",
-        ">> Circuit prepared for a simulated measurement: ", circuit,
+        ">> Circuit prepared for a simulated measurement: {:?}",
+        circuit,
     );
 
     let backend = Backend::new(2);
@@ -96,33 +91,34 @@ pub fn measuring_observables() {
     circuit.add_operation(CNOT::new(0, 1));
     circuit.add_operation(PragmaRepeatedMeasurement::new("ro".to_string(), 10, None));
 
-    let mut measurement_input = BasisRotationInput::new(2, false);
+    let mut measurement_input = PauliZProductInput::new(2, false);
     // From readout 'ro' measure two pauli products 0: < Z0 > and 1: < Z0 Z1 >
-    measurement_input.add_pauli_product("ro".to_string(), vec![0]).unwrap();
-    measurement_input.add_pauli_product("ro".to_string(), vec![0, 1]).unwrap();
+    measurement_input
+        .add_pauliz_product("ro".to_string(), vec![0])
+        .unwrap();
+    measurement_input
+        .add_pauliz_product("ro".to_string(), vec![0, 1])
+        .unwrap();
     // One expectation value: 3 * pauli_product0 + 1 * pauli_product1
     measurement_input
         .add_linear_exp_val("example".to_string(), HashMap::from([(0, 3.0), (1, 1.0)]))
         .unwrap();
-    println!(
-        "{} {:?}",
-        ">> Measurement input defined: ", measurement_input,
-    );
+    println!(">> Measurement input defined: {:?}", measurement_input,);
 
-    let measurement = BasisRotation {
+    let measurement = PauliZProduct {
         input: measurement_input,
         circuits: vec![circuit.clone()],
         constant_circuit: None,
     };
     let backend = Backend::new(2);
-    let program = QuantumProgram::BasisRotation {
-        measurement: measurement,
+    let program = QuantumProgram::PauliZProduct {
+        measurement,
         input_parameter_names: vec![],
     };
 
     // Measurement result
     let result = program.run(backend, &[]).unwrap().unwrap()["example"];
-    println!("{} {:?}", ">> Result of Quantum Program: ", result);
+    println!(">> Result of Quantum Program: {:?}", result);
 
     // Validation check
     assert!(result > -4.0 * 10.0);
@@ -140,41 +136,42 @@ pub fn serialization_quantum_program() {
     circuit.add_operation(PauliX::new(0));
     circuit.add_operation(CNOT::new(0, 1));
     circuit.add_operation(PragmaRepeatedMeasurement::new("ro".to_string(), 10, None));
-    
-    let mut measurement_input = BasisRotationInput::new(2, false);
+
+    let mut measurement_input = PauliZProductInput::new(2, false);
     // From readout 'ro' measure two pauli products 0: < Z0 > and 1: < Z0 Z1 >
-    measurement_input.add_pauli_product("ro".to_string(), vec![0]).unwrap();
-    measurement_input.add_pauli_product("ro".to_string(), vec![0, 1]).unwrap();
+    measurement_input
+        .add_pauliz_product("ro".to_string(), vec![0])
+        .unwrap();
+    measurement_input
+        .add_pauliz_product("ro".to_string(), vec![0, 1])
+        .unwrap();
     // One expectation value: 3 * pauli_product0 + 1 * pauli_product1
     measurement_input
         .add_linear_exp_val("example".to_string(), HashMap::from([(0, 3.0), (1, 1.0)]))
         .unwrap();
-    println!(
-        "{} {:?}",
-        ">> Measurement input defined: ", measurement_input,
-    );
-    
-    let measurement = BasisRotation {
+    println!(">> Measurement input defined: {:?}", measurement_input,);
+
+    let measurement = PauliZProduct {
         input: measurement_input,
         circuits: vec![circuit.clone()],
         constant_circuit: None,
     };
 
-    let program = QuantumProgram::BasisRotation {
+    let program = QuantumProgram::PauliZProduct {
         measurement: measurement.clone(),
         input_parameter_names: vec![],
     };
 
-    // First, let's de-/serialize the BasisRotation measurement and test the outcome
+    // First, let's de-/serialize the PauliZProduct measurement and test the outcome
     let measurement_json = serde_json::to_string(&measurement).unwrap();
-    let measurement_new: BasisRotation = serde_json::from_str(&measurement_json).unwrap();
+    let measurement_new: PauliZProduct = serde_json::from_str(&measurement_json).unwrap();
     assert!(measurement_new == measurement);
-    println!(">> De/Serialization of BasisRotation performed successfully.");
+    println!(">> De/Serialization of PauliZProduct performed successfully.");
 
     // Next, de-/serialize the Quantum Program
     let program_json = serde_json::to_string(&program).unwrap();
     let program_new: QuantumProgram = serde_json::from_str(&program_json).unwrap();
-    assert!(program == program_new.clone());
+    assert!(program == program_new);
     println!(">> De/Serialization of QuantumProgram performed successfully.");
     println!(">> Introduction example end.")
 }
