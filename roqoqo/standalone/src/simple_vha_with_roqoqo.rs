5c8b24ce2bfa447106bd7b3290ba4d180365126e
Marina Walt <marina.walt@quantumsimulations.com>
2022-05-13 10:14:54 +0000

    draft vha example prepared. optimization to be added

--- roqoqo/standalone/src/simple_vha_with_roqoqo.rs
+++ roqoqo/standalone/src/simple_vha_with_roqoqo.rs
@@ -3,11 +3,24 @@ extern crate eigenvalues;
 extern crate nalgebra;
 extern crate ndarray;
 extern crate num_complex;
+extern crate roqoqo_quest;
 
+use roqoqo::{Circuit, operations::*, QuantumProgram};
+// for measuring observables
+use roqoqo::measurements::{PauliZProductInput, PauliZProduct};
+// simulation and measurement of the circuit is handled by the QuEST interface
+use roqoqo_quest::Backend;
+use qoqo_calculator::CalculatorFloat;
+use std::collections::HashMap;
+use std::vec::*;
+
+use ndarray::{array, Array, Array1, Array2, ArrayView1};
 use nalgebra as na;
 use na::ComplexField;
-use ndarray::{array, Array2};
 use num_complex::Complex64;
+
+use argmin::prelude::*;
+use argmin::solver::conjugategradient::ConjugateGradient;
 
 /// Example how to run a simple variational algorithm with roqoqo
 ///
@@ -19,6 +32,300 @@ use num_complex::Complex64;
 /// see the literature (e.g. http://arxiv.org/abs/1304.3061, http://arxiv.org/abs/1509.04279).
 ///
 pub fn run_simple_vha() {
+
+    // PART I: Unitary time evolution
+    //
+    // We construct circuits that apply time evolution under the even and odd hopping Hamiltonians
+    // and under the magnetic field using variables t (hopping_parameter) and B (magnetic_field).  
+    // For each iteration of the evolution we get free symbolic parameters theta_even_i, theta_odd_i and theta_z_i.
+
+    // Helper function to return a circuit for the time-like evolution under even-to-odd hopping.
+    //
+    // # Arguments
+    // * thetasymb: symbolic parameter 'theta' of the even-to-odd time-like evolution.
+    // * number_qubits: number of qubits in the system.
+    // * hopping_parameter: parameter 't' for the hopping term of the Hamiltonian.
+    fn create_even_hopping_circuit(thetasymb: CalculatorFloat, number_qubits: usize, hopping_parameter: f64) -> Circuit {
+        let mut circuit = Circuit::new();
+
+        // Representation of the \sigma^x\sigma^x interaction between two spins by the CNOT and Rotation gates
+        for k in (0..number_qubits-1).step_by(2) {
+            // Basis rotation into z-basis
+            circuit.add_operation(Hadamard::new(k));
+            circuit.add_operation(Hadamard::new(k+1));
+            // Interaction
+            circuit.add_operation(CNOT::new(k+1, k));
+            circuit.add_operation(RotateZ::new(k, (thetasymb.clone() * hopping_parameter).to_string().into()));
+            circuit.add_operation(CNOT::new(k+1, k));
+            // Basis rotation back into x-basis
+            circuit.add_operation(Hadamard::new(k));
+            circuit.add_operation(Hadamard::new(k+1))
+        }
+
+        circuit
+    }
+
+    // Helper function to return a circuit for the time-like evolution under odd-to-even hopping.
+    //
+    // # Arguments
+    // * thetasymb: symbolic parameter 'theta' of the odd-to-even time-like evolution.
+    // * number_qubits: number of qubits in the system.
+    // * hopping_parameter: parameter 't' for the hopping term of the Hamiltonian.
+    fn create_odd_hopping_circuit(thetasymb: CalculatorFloat, number_qubits: usize, hopping_parameter: f64) -> Circuit {
+        let mut circuit = Circuit::new();
+
+        // Representation of the \sigma^x\sigma^x interaction between two spins by the CNOT and Rotation gates
+        for k in (1..number_qubits-1).step_by(2) {
+            // Basis rotation into z-basis
+            circuit.add_operation(Hadamard::new(k));
+            circuit.add_operation(Hadamard::new(k+1));
+            // Interaction
+            circuit.add_operation(CNOT::new(k+1, k));
+            circuit.add_operation(RotateZ::new(k, (thetasymb.clone() * hopping_parameter).to_string().into()));
+            circuit.add_operation(CNOT::new(k+1, k));
+            // Basis rotation back into x-basis
+            circuit.add_operation(Hadamard::new(k));
+            circuit.add_operation(Hadamard::new(k+1))
+        }
+
+        // Periodic boundary conditions
+        circuit.add_operation(Hadamard::new(number_qubits-1));
+        circuit.add_operation(Hadamard::new(0));
+        circuit.add_operation(CNOT::new(0, number_qubits - 1));
+        circuit.add_operation(RotateZ::new(number_qubits - 1, (thetasymb * hopping_parameter).to_string().into()));
+        circuit.add_operation(CNOT::new(0, number_qubits - 1));
+        circuit.add_operation(Hadamard::new(number_qubits-1));
+        circuit.add_operation(Hadamard::new(0));
+
+        circuit
+    }
+
+    // Helper function to return a circuit for the time-like evolution under magnetic field.
+    //
+    // # Arguments
+    // * thetasymb: symbolic parameter 'theta' of the z-rotation.
+    // * number_qubits: number of qubits in the system
+    // * magnetic_field: parameter 'B' for the magnetic term of the Hamiltonian.
+    fn create_magnetic_field_circuit(thetasymb: CalculatorFloat, number_qubits: usize, magnetic_field: f64) -> Circuit {
+        let mut circuit = Circuit::new();
+
+        // Representation of the \sigma^x\sigma^x interaction between two spins by the CNOT and Rotation gates
+        for k in 0..number_qubits {
+            circuit.add_operation(RotateZ::new(k, (thetasymb.clone() * magnetic_field).to_string().into()));
+        }
+
+        circuit
+    }
+
+    /// Function to construct a Circuit for the unitary evolution.
+    ///
+    /// Arguments
+    /// * iter_evolution: number of iterations of evolution, minimum 1.
+    /// * number_qubits: number of qubits in the system
+    /// * hopping_parameter: parameter 't' for the hopping term of the Hamiltonian.
+    /// * magnetic_field: parameter 'B' for the magnetic term of the Hamiltonian.
+    ///
+    /// # Returns
+    /// Circuit for the unitary evolution
+    pub fn create_evolution_circuit(
+        iter_evolution: usize,
+        number_qubits: usize,
+        hopping_parameter: f64,
+        magnetic_field: f64,
+    ) -> Circuit {
+        let mut circuit = Circuit::new();
+
+        // here: theta_even_i, theta_odd_i and theta_z_i are symbolic parameters (free variational parameters)
+        for i in 0..iter_evolution {
+            circuit += create_even_hopping_circuit(
+                ("theta_even_".to_string() + &i.to_string()).into(),
+                number_qubits,
+                hopping_parameter,
+            );
+            circuit += create_odd_hopping_circuit(
+                ("theta_odd_".to_string() + &i.to_string()).into(),
+                number_qubits,
+                hopping_parameter,
+            );
+            circuit += create_magnetic_field_circuit(
+                ("theta_z_".to_string() + &i.to_string()).into(),
+                number_qubits,
+                magnetic_field,
+            );
+        }
+
+        circuit
+    }
+
+    // Combining the parts to QuantumProgram
+    //
+    // To execute the optimization we combine all the constructed circuits
+    // and put them in one qoqo quantum programm.
+    // With QuantumProgram we only need to provide the free parameters
+    // and get back the measured expectation values. 
+
+    // Finally, all the previous steps are collected in a wrapper_function
+    // which takes free parameters as argument and returns a single value as result.
+    // The wrapper_function can be used to run the optimization routine.
+    fn wrapper_function(theta: &[f64]) -> f64 {
+        // Seting up initial state vector
+        let initial_statevector: Array1<Complex64> = array![
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0/6_f64.sqrt(), 0.0),
+            Complex64::new(1.0/6_f64.sqrt(), 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0/6_f64.sqrt(), 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(0.0, 0.0),
+            Complex64::new(1.0/2_f64.sqrt(), 0.0),
+        ];
+    
+        let mut circuit_init = Circuit::new();
+    
+        circuit_init += PragmaSetStateVector::new(initial_statevector);
+    
+        println!("Step 1: Initialization circuit constructed.");
+    
+        // variables and parameters
+        let number_qubits: usize = 3;
+        let magnetic_field: f64 = 1.0;
+        let hopping_parameter: f64 = 3.0;
+        // In order to achieve better minimization results we default to several iterations of (pseudo) time-evolution
+        let iter_evolution: usize = 4;
+        // Construction the evolution circuit
+        let circuit_evolution: Circuit = create_evolution_circuit(iter_evolution, number_qubits, hopping_parameter, magnetic_field);
+    
+        println!("Step 2: Constructed evolution circuit.");
+    
+    
+        // Setting up two basis rotation measurement circuits since we need to measure in two different bases.
+        let mut circuit_x_basis_measurement = Circuit::new();
+        let mut circuit_z_basis_measurement = Circuit::new();
+    
+        // Adding 'DefinitionBit' to the circuit defines the classical bit registers used to store the qubit readout.
+        circuit_x_basis_measurement += DefinitionBit::new("ro_x".to_string(), number_qubits, true);
+        circuit_z_basis_measurement += DefinitionBit::new("ro_z".to_string(), number_qubits, true);
+    
+        // Basis rotation with the Hadamard gate: Bring all qubits into z-basis.
+        for i in 0..number_qubits {
+            circuit_x_basis_measurement.add_operation(Hadamard::new(i));
+        };
+    
+        // Add measurement operation to all circuits to write the measured values into the classical registers.
+        let number_measurements: usize = 10000;
+        circuit_z_basis_measurement.add_operation(PragmaRepeatedMeasurement::new("ro_z".to_string(), number_measurements, None));
+        circuit_x_basis_measurement.add_operation(PragmaRepeatedMeasurement::new("ro_x".to_string(), number_measurements, None));
+    
+        // Setting up measurement input determining which expectation values of PauliProducts are measured from which circuit
+        // and how they are combined linearly
+        let mut measurement_input = PauliZProductInput::new(number_qubits, false);
+        measurement_input.add_pauliz_product("ro_z".to_string(), vec![0]).unwrap();
+        measurement_input.add_pauliz_product("ro_z".to_string(), vec![1]).unwrap();
+        measurement_input.add_pauliz_product("ro_z".to_string(), vec![2]).unwrap();
+        measurement_input.add_pauliz_product("ro_x".to_string(), vec![0,1]).unwrap();
+        measurement_input.add_pauliz_product("ro_x".to_string(), vec![1,2]).unwrap();
+        measurement_input.add_pauliz_product("ro_x".to_string(), vec![0,2]).unwrap();
+        // Defining and adding the linear combinations of Pauli products that give the expectation values
+        let linear_exp_val = HashMap::from([
+            (0, magnetic_field),
+            (1, magnetic_field),
+            (2, magnetic_field),
+            (3, hopping_parameter),
+            (4, hopping_parameter),
+            (5, hopping_parameter),
+        ]);
+        measurement_input.add_linear_exp_val("energy".to_string(), linear_exp_val).unwrap();
+    
+        println!("Step 3: Measurement circuits constructed. Measurement input prepared.");
+        println!("In z-basis: {:?}", circuit_z_basis_measurement);
+        println!("In x-basis: {:?}", circuit_x_basis_measurement);
+    
+        // Construct basis rotation measurement to get the expectation values.
+        let measurement = PauliZProduct {
+            input: measurement_input,
+            circuits: vec![
+                circuit_init.clone() + circuit_evolution.clone() + circuit_z_basis_measurement.clone(),
+                circuit_init.clone() + circuit_evolution + circuit_x_basis_measurement.clone(),
+            ],
+            constant_circuit: None,
+        };
+    
+        // One needs to define a backend where the program is executed.
+        let backend = Backend::new(number_qubits);
+        // QuantumProgram takes the prepared list of circuits and the list of free parameter names (the symbolic values in the circuit).
+        let program = QuantumProgram::PauliZProduct {
+            measurement,
+            input_parameter_names: vec![
+                "theta_even_0".to_string(),"theta_odd_0".to_string(), "theta_z_0".to_string(),
+                "theta_even_1".to_string(),"theta_odd_1".to_string(), "theta_z_1".to_string(),
+                "theta_even_2".to_string(),"theta_odd_2".to_string(), "theta_z_2".to_string(),
+                "theta_even_3".to_string(),"theta_odd_3".to_string(), "theta_z_3".to_string(),
+            ],
+        };
+    
+        println!("Step 4: QuantumProgram constructed.");
+    
+        program.run(backend, theta).unwrap().unwrap()["energy"]
+    }
+
+    // Optimization of free parameters
+    // Minimiization routine to optimize the free parameters.
+    // The wrapper_function consisting of the quantum program has 12 free parameters:
+    // theta_even, theta_odd and theta_z for each of the 4 iterations of evolution.
+    let theta = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+    println!("{:?}", wrapper_function(&theta));
+
+    // Copyright 2018-2020 argmin developers
+    //
+    // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+    // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+    // http://opensource.org/licenses/MIT>, at your option. This file may not be
+    // copied, modified, or distributed except according to those terms.
+
+    // Example for ConjugateGradient solver from Github
+    struct MyProblem {}
+
+    impl ArgminOp for MyProblem {
+        type Param = Vec<f64>;
+        type Output = Vec<f64>;
+        type Hessian = ();
+        type Jacobian = ();
+        type Float = f64;
+
+        fn apply(&self, p: &Vec<f64>) -> Result<Vec<f64>, Error> {
+            Ok(vec![4.0 * p[0] + 1.0 * p[1], 1.0 * p[0] + 3.0 * p[1]])
+        }
+    }
+
+    fn run() -> Result<(), Error> {
+        // Define inital parameter vector
+        let init_param: Vec<f64> = vec![2.0, 1.0];
+
+        // Define the right hand side `b` of `A * x = b`
+        let b = vec![1.0, 2.0];
+
+        // Set up operator
+        let operator = MyProblem {};
+
+        // Set up the solver
+        let solver: ConjugateGradient<_, f64> = ConjugateGradient::new(b)?;
+
+        // Run solver
+        let res = Executor::new(operator, solver, init_param)
+            .add_observer(ArgminSlogLogger::term(), ObserverMode::Always)
+            .max_iters(2)
+            .run()?;
+
+        // Wait a second (lets the logger flush everything before printing to screen again)
+        std::thread::sleep(std::time::Duration::from_secs(1));
+
+        // Print result
+        println!("{}", res);
+        Ok(())
+    }
+
+    println!("{:?}", run());
+
     //PART II: Compare the calculated (approximate) result to the exact classical solution
     //
     // Here we present an exact solution of the sample Hamiltonian to compare the exact results
